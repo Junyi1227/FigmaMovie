07ad1d9294fa6ab258c68ffda519846729436170
Jimmy <trance1227@gmail.com>
2020-11-03 10:29:16 +0800

    Initial Commit

---
+++ FigmaMovie/FigmaMovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  FigmaMovieApp.swift
+//  FigmaMovie
+//
+//  Created by Junyi Wang on 2020/11/3.
+//
+
+import SwiftUI
+
+@main
+struct FigmaMovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
